3bd44cbce1a47ab4716b456769a65d6a62fe4e59
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2024-06-10 22:31:29 -0300

    headline.js criado

--- JSCodebox/easy/dom-manipulation/headline/headline.js
+++ JSCodebox/easy/dom-manipulation/headline/headline.js
@@ -1,6 +1,21 @@
 function printHeadline(headline) {
-    let h1 = document.getElementById('id');
-    h1.innerHTML = headline;
+    let h1 = document.createElement('h1')
+    document.body.appendChild(h1)
+    h1.innerHTML = headline
 }
 
-printHeadline('Teste')
+// ou
+
+function printHeadline1(headline) {
+    document.write(`<h2>${headline}</h2>`)
+}
+
+// ou
+
+function printHeadline2(headline) {
+    document.body.innerHTML += `<h3>${headline}</h3>`;
+}
+
+printHeadline('teste')
+printHeadline1('teste1')
+printHeadline2('teste2')
