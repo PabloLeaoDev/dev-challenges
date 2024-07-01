3d095c7c6c1ecd549eb394e3320c89b93918c801
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2024-06-30 21:37:12 -0300

    firstHello.js criado

---
+++ JSCodebox/medium/strings/firstHello.js
@@ -0,0 +1,52 @@
+function firstHello(sentence) {
+    const helloArr = ['H', 'e', 'l', 'l', 'o'];
+    let helloIdx = 0;
+    for (let i = 0; i < helloArr.length; i++) {
+        if (sentence[helloIdx] === helloArr[i]) {
+            helloIdx++;
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
+// ou
+
+function firstHello1(sentence) {
+    return sentence.startsWith('Hello')
+}
+
+// ou
+
+function firstHello2(sentence) {
+    const sp = sentence.split(' ');
+    if(sp[0].match(/Hello/)) {
+        return true;
+    }
+    return false;
+}
+
+// ou
+
+function firstHello(sentence) {
+    let word ="Hello";
+    if (sentence.slice(0, word.length)==="Hello") {return true;}
+    else {return false;}
+}
+
+// ou
+
+function firstHello(sentence) {
+    sentence.trim()
+    return sentence.indexOf("Hello")===0
+}
+
+// ou
+
+function firstHello(sentence) {
+    return sentence.substring(0, 5)==="Hello" ? true: false;;
+}
+
+console.log(firstHello('Hello, World!'));
+console.log(firstHello('Hell, yeah!'));
