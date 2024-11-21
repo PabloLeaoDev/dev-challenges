bcfd2a646b55a6908b97feae6794725ef6face59
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2024-11-21 19:30:12 -0300

    react-base updated

--- JSCodebox/test/fullstackCurseExc/react/react-base/src/App.js
+++ JSCodebox/test/fullstackCurseExc/react/react-base/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
-import Login from './pages/Login';
+import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyles';
+import Header from './components/Header';
+import CustomRouter from './routes';
 
 function App() {
   return (
-    <>
-      <Login />
+    <BrowserRouter>
+      <Header />
+      <CustomRouter />
       <GlobalStyle />
-    </>
+    </BrowserRouter>
   );
 }
 
