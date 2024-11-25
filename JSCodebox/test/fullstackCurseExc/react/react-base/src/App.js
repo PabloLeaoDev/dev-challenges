14244841f3da7567bdb24e9762dd52fe8a08ea68
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2024-11-25 16:20:01 -0300

    react-base updated

--- JSCodebox/test/fullstackCurseExc/react/react-base/src/App.js
+++ JSCodebox/test/fullstackCurseExc/react/react-base/src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './store';
+import store, { persistor } from './store';
 import GlobalStyle from './styles/GlobalStyles';
 import Header from './components/Header';
 import CustomRouter from './routes';
@@ -12,12 +13,14 @@ function App() {
   return (
     <>
       <Provider store={store}>
-        <BrowserRouter>
-          <Header />
-          <CustomRouter />
-          <GlobalStyle />
-          <ToastContainer autoClose={3000} className={'toast-container'} />
-        </BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <BrowserRouter>
+            <Header />
+            <CustomRouter />
+            <GlobalStyle />
+            <ToastContainer autoClose={3000} className={'toast-container'} />
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
     </>
   );
--- JSCodebox/test/fullstackCurseExc/react/react-base/src/store/modules/example/reducer.js
+++ JSCodebox/test/fullstackCurseExc/react/react-base/src/store/modules/example/reducer.js
@@ -1,3 +1,5 @@
+import * as types from '../types';
+
 const initialState = {
   clickedButton: false
 };
@@ -5,10 +7,17 @@ const initialState = {
 export default function reducer(state = initialState, action) {
   let newState;
   switch (action.type) {
-    case 'CLICKED_BUTTON':
+    case types.CLICKED_BUTTON_SUCCESS:
+      console.log('Success');
       newState = { ...state };
       newState.clickedButton = !newState.clickedButton;
       return newState;
+    case types.CLICKED_BUTTON_REQUEST:
+      console.log('Request in progress');
+      return state;
+    case types.CLICKED_BUTTON_FAILURE:
+      console.log('An error occurred');
+      return state;
     default:
       return state;
   }
