cdc8434f2b072fba683bae863309655c342f3694
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2024-12-23 15:13:27 -0300

    index.ts updated

--- JSCodebox/base-build/to-do-list/src/index.ts
+++ JSCodebox/base-build/to-do-list/src/index.ts
@@ -1,66 +1,125 @@
-(function () {
+(() => {
   let btn: NodeListOf<HTMLButtonElement>;
   let arrBtn: HTMLButtonElement[];
-  let currentBtn: EventTarget | null;
-  let qtdBtn: number;
-  let flagAdd: boolean;
-  let flagRemove: boolean;
+  let currentBtn: HTMLButtonElement;
+  let tasks: Array<HTMLLIElement> = [];
+  let id: number = 0;
+
+  enum Buttons {
+    Add,
+    Remove,
+    Update
+  }
+
+  let buttonFlag: Buttons;
 
   document.addEventListener('click', (e) => {
     btn = document.querySelectorAll('.btn');
     arrBtn = Array.from(btn);
 
-    flagAdd = false;
-    flagRemove = false;
     if (e.target) {
       if (e.target.toString() !== '[object HTMLButtonElement]') return;
     }
-    currentBtn = e.target;
+
+    currentBtn = e.target as HTMLButtonElement;
     if (currentBtn === arrBtn[0]) {
-      flagAdd = true
-    } else {
-      flagRemove = true;
+      buttonFlag = Buttons.Add;
+    } else if (isOdd(arrBtn.indexOf(currentBtn))) {
+      buttonFlag = Buttons.Update;
+    } else if (!isOdd(arrBtn.indexOf(currentBtn))) {
+      buttonFlag = Buttons.Remove;
     }
 
-    if (flagAdd) {
-      qtdBtn = btn.length - 1;
-      main(e.target, qtdBtn);
+    if (buttonFlag === Buttons.Add) {
+      id++;
     }
+    main(buttonFlag);
 
-    if (flagRemove) {}
+    function main(buttonFlag: Buttons): void {
+      const inputTask = document.querySelector('input[type="text"]') as HTMLInputElement;
+      const taskUl = document.querySelector('.taskList') as HTMLUListElement;
+
+      if (!(inputTask && taskUl)) return;
+
+      switch (buttonFlag) {
+        case Buttons.Add:
+          if (!inputTask.value) {
+            alert('Por favor, insira uma tarefa.');
+            return;
+          }
+          const newTask = createTask(inputTask.value, id);
+          tasks.push(newTask);
+          inputTask.value = '';
+          break;
+        case Buttons.Update:
+          if (!inputTask.value) {
+            alert('Por favor, insira uma tarefa.');
+            return;
+          }
+          updateTask(tasks, currentBtn, inputTask.value, id);
+          inputTask.value = '';
+          break;
+        case Buttons.Remove:
+          const removedTask = removeTask(tasks, currentBtn);
+          tasks = [...removedTask];
+          break;
+      }
+
+      taskUl.innerHTML = '';
+      tasks.forEach((liEl) => {
+        taskUl.appendChild(liEl);
+      });
+    }
   });
 })();
 
-function main(event: EventTarget | null, id: number) {
-  const inputTask = document.querySelector('input[type="text"]') as HTMLInputElement;
-  const taskList = document.querySelector('.taskList') as HTMLUListElement;
 
-  if (!(inputTask && taskList)) return;
-
-  if (!inputTask.value) {
-    alert('Por favor, insira uma tarefa.');
-    return;
-  }
-
-  switch (event) {
-    case event:
-      taskList.appendChild(createTask(inputTask.value, id));
-      inputTask.value = '';
-      break;
-    case event:
-
-      break;
-  }
+function createTask(taskName: string, id?: number): HTMLLIElement {
+  const liEl: HTMLLIElement = document.createElement('li');
+  liEl.innerText = `${taskName} `;
+  const btnUpdateEl: HTMLButtonElement = createButton('update', id);
+  liEl.appendChild(btnUpdateEl);
+  const btnRemoveEl: HTMLButtonElement = createButton('remove', id);
+  liEl.appendChild(btnRemoveEl);
+  return liEl;
 }
 
-function createTask(taskName: string, id?: number): HTMLElement {
-  const liEl: HTMLElement = document.createElement('li');
-  liEl.innerText = `${taskName} `;
-  const btnEl: HTMLElement = document.createElement('button');
-  btnEl.setAttribute('type', 'button');
-  btnEl.classList.add('btn');
-  btnEl.classList.add(`remove${id}`);
-  btnEl.innerText = 'Remove';
-  liEl.appendChild(btnEl);
-  return liEl;
+function updateTask(tasks: HTMLLIElement[], target: HTMLElement, content: string, id?: number): HTMLLIElement[] {
+  const tasksCopy = [...tasks];
+  let taskForUpdate: HTMLElement;
+  for (const taskIndex in tasksCopy) {
+    taskForUpdate = tasksCopy[taskIndex];
+    if (taskForUpdate === target.parentElement) {
+        taskForUpdate.innerHTML = `${content} <button type="button" class="btn update${id}">Update</button><button type="button" class="btn remove${id}">Remove</button>`;
+    }
+  }
+  return tasksCopy;
+}
+
+function removeTask(tasks: HTMLLIElement[], target: HTMLElement): HTMLLIElement[] {
+  const tasksCopy = [...tasks];
+  for (const taskIndex in tasksCopy) {
+    if (tasksCopy[taskIndex] === target.parentElement) tasksCopy.splice(Number(taskIndex), 1);
+  }
+  return tasksCopy;
+}
+
+
+function createButton(className: string, id?: number): HTMLButtonElement {
+  const button: HTMLButtonElement = document.createElement('button');
+  button.setAttribute('type', 'button');
+  button.classList.add('btn');
+  button.classList.add(`${className}${id}`);
+  className = capitalizeFirstLetter(className);
+  button.innerText = `${className}`;
+  return button;
+}
+
+function capitalizeFirstLetter(value: string) {
+  return String(value).charAt(0).toUpperCase() + String(value).slice(1);
+}
+
+function isOdd(value: number): boolean {
+  if (value % 2 === 0) return false;
+  return true;
 }
